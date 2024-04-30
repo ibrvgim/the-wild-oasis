77ca982f862142c0136deabeaae6721c43503785
ibrvgim <ibrv.cvrtxxn@gmail.com>
2024-04-30 17:45:29 +0200

    small updates

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # THE WILD OASIS
+
+The Wild Oasis is designed to empower employees to manage the progress of their business. This is similar to the admin panel.
+
+> [!IMPORTANT]
+> The Website does not have responsive design.
+
+> [!NOTE]
+> New users might only register within the website.
+
+## OVERVIEW
+
+### Technologies used to build
+
+- Vite - https://vitejs.dev/
+- ReactJS - https://react.dev/
+- React Router - https://reactrouter.com/en/main
+- TanStack Query - https://tanstack.com/query/latest
+- React Hook Form - https://react-hook-form.com/
+- React Hot Toast - https://react-hot-toast.com/
+- React Icons - https://react-icons.github.io/react-icons/
+
+For the server part SUPABASE was used: https://supabase.com/
+
+### Website link
+
+[THE WILD OASIS](https://okka.vercel.app/)
+
+### Setup
+
+Manage locally:
+
+```
+npm install && npm run dev
+```
+
+## AUTHOR
+
+Created by - Ibrahim Ismayilov.
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { Suspense, lazy } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
-import ProtectedRoute from "./ui/ProtectedRoute";
-import { DarkModeProvider } from "./context/DarkModeContext";
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+import ProtectedRoute from './ui/ProtectedRoute';
+import { DarkModeProvider } from './context/DarkModeContext';
 
-const AppLayout = lazy(() => import("./ui/AppLayout"));
-const Dashboard = lazy(() => import("./pages/Dashboard"));
-const Bookings = lazy(() => import("./pages/Bookings"));
-const Cabins = lazy(() => import("./pages/Cabins"));
-const Users = lazy(() => import("./pages/Users"));
-const Settings = lazy(() => import("./pages/Settings"));
-const Account = lazy(() => import("./pages/Account"));
-const Login = lazy(() => import("./pages/Login"));
-const PageNotFound = lazy(() => import("./pages/PageNotFound"));
-const Booking = lazy(() => import("./pages/Booking"));
-const Checkin = lazy(() => import("./pages/Checkin"));
+const AppLayout = lazy(() => import('./ui/AppLayout'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Bookings = lazy(() => import('./pages/Bookings'));
+const Cabins = lazy(() => import('./pages/Cabins'));
+const Users = lazy(() => import('./pages/Users'));
+const Settings = lazy(() => import('./pages/Settings'));
+const Account = lazy(() => import('./pages/Account'));
+const Login = lazy(() => import('./pages/Login'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound'));
+const Booking = lazy(() => import('./pages/Booking'));
+const Checkin = lazy(() => import('./pages/Checkin'));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,7 +30,6 @@ function App() {
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
         <GlobalStyles />
         <BrowserRouter>
           <Suspense>
@@ -43,27 +41,27 @@ function App() {
                   </ProtectedRoute>
                 }
               >
-                <Route index element={<Navigate replace to="dashboard" />} />
-                <Route path="dashboard" element={<Dashboard />} />
-                <Route path="bookings" element={<Bookings />} />
-                <Route path="bookings/:bookingId" element={<Booking />} />
-                <Route path="checkin/:bookingId" element={<Checkin />} />
-                <Route path="cabins" element={<Cabins />} />
-                <Route path="users" element={<Users />} />
-                <Route path="settings" element={<Settings />} />
-                <Route path="account" element={<Account />} />
+                <Route index element={<Navigate replace to='dashboard' />} />
+                <Route path='dashboard' element={<Dashboard />} />
+                <Route path='bookings' element={<Bookings />} />
+                <Route path='bookings/:bookingId' element={<Booking />} />
+                <Route path='checkin/:bookingId' element={<Checkin />} />
+                <Route path='cabins' element={<Cabins />} />
+                <Route path='users' element={<Users />} />
+                <Route path='settings' element={<Settings />} />
+                <Route path='account' element={<Account />} />
               </Route>
 
-              <Route path="login" element={<Login />} />
-              <Route path="*" element={<PageNotFound />} />
+              <Route path='login' element={<Login />} />
+              <Route path='*' element={<PageNotFound />} />
             </Routes>
           </Suspense>
         </BrowserRouter>
 
         <Toaster
-          position="top-center"
+          position='top-center'
           gutter={12}
-          containerStyle={{ margin: "8px" }}
+          containerStyle={{ margin: '8px' }}
           toastOptions={{
             success: {
               duration: 3000,
@@ -74,11 +72,11 @@ function App() {
             },
 
             style: {
-              fontSize: "16px",
-              maxWidth: "500px",
-              padding: "16px 24px",
-              backgroundColor: "var(--color-grey-0)",
-              color: "var(--color-grey-700)",
+              fontSize: '16px',
+              maxWidth: '500px',
+              padding: '16px 24px',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-700)',
             },
           }}
         />
